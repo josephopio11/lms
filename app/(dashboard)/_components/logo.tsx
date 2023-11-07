104a312291eee036718de2c787a04ca5e701069b
Joseph <josephopio11@gmail.com>
2023-11-07 23:00:42 +0800

    For the night

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 import React from 'react'
 
 const Logo = () => {
-    return (
-        <Image src="/next.svg" alt="logo" width={100} height={100} />
-    )
+  return (
+    <Image src="/next.svg" alt="logo" width={100} height={100} />
+  )
 }
 
 export default Logo
