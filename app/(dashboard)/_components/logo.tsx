f0016775b9499b21a8b634cc70f98ff0ac8af4b3
Joseph Opio <josephopio11@gmail.com>
2023-11-08 08:46:01 +0000

    resolved "@radix-ui/react-dialog": "^1.0.5", issue

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Logo = () => {
   return (
-    <Image src="/next.svg" alt="logo" width={100} height={100} />
+    <Link href={"/"}>
+      <Image src="/next.svg" alt="logo" width={100} height={100} />
+    </Link>
   )
 }
 
