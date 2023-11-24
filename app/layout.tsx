da842d1f6968b000d540f8f8a72ac8aa01b752ca
Joseph Opio <josephopio11@gmail.com>
2023-11-24 00:13:36 +0000

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,48 @@ import Link from 'next/link'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'fLEx - Focused Learning Experience',
-  description: 'Created by Joseph Opio ',
+  metadataBase: new URL('https://lms.josephopio.com'),
+  title: {
+    template: '%s | Focused Learning Experience',
+    default: 'fLEx | Focused Learning Experience',
+  },
+  keywords: ["fLEx", "focused learning experience", "online courses", "elearning", "online school", "free courses", "joseph", "opio", "computer", "science", "teacher", "gems", "xcl", "igcse", "cambridge", "programming", "malaysia", "bxcl", "ugandan teacher", "ugandan in malaysia"],
+  description: 'Here, you will find courses curated for you. Some are free and some are paid. You can get in touch with me so that you also create your own courses and share them with others. You will then be able to earn from this site.',
+  robots: 'index, follow',
+  alternates: {
+    canonical: '/',
+    languages: {
+      'en': '/',
+    }
+  },
+  // TODO: Thi sis where i need to start editing
+
+  openGraph: {
+    url: '/',
+    title: {
+      template: '%s | Focused Learning Experience',
+      default: 'fLEx | Focused Learning Experience',
+    },
+    description: 'Here, you will find courses curated for you. Some are free and some are paid. You can get in touch with me so that you also create your own courses and share them with others. You will then be able to earn from this site.',
+    images: [
+      {
+        url: '/fLEx.svg',
+        width: 800,
+        height: 800,
+        alt: 'fLEx - Focused Learning Experieince',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+    siteName: 'fLEx - Focused Learning Experieince',
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@site",
+    creator: "@creator",
+    images: "/fLEx.svg",
+    description: 'Here, you will find courses curated for you. Some are free and some are paid. You can get in touch with me so that you also create your own courses and share them with others. You will then be able to earn from this site.'
+  }
 }
 
 export default function RootLayout({
